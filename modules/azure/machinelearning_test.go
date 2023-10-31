ffaf36c0c285a558d342bd29ad8de4d470bc4f0c
Hattan Shobokshi <hattan.shobokshi@microsoft.com>
2023-10-30 19:28:26 -0700

    Add machine learning unit tests

--- modules/azure/machinelearning_test.go
+++ modules/azure/machinelearning_test.go
@@ -1,1 +1,28 @@
+//go:build azure
+// +build azure
+
 package azure
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestMachinelearningWorkspaceExistsE(t *testing.T) {
+	t.Parallel()
+
+	subscriptionID := ""
+
+	_, err := MachinelearningWorkspaceExistsE(subscriptionID)
+	require.Error(t, err)
+}
+
+func TestGetMachinelearningWorkspaceE(t *testing.T) {
+	t.Parallel()
+
+	subscriptionID := ""
+
+	_, err := GetMachinelearningWorkspaceE(subscriptionID)
+	require.Error(t, err)
+}
