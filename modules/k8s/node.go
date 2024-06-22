511d77a94dc6c465f2b913e6beb87e86ea2746ad
Matt Calhoun <matt@mattcalhoun.com>
2024-06-22 15:58:13 -0400

    add eks support and k8s node assertions

--- modules/k8s/node.go
+++ modules/k8s/node.go
@@ -3,6 +3,7 @@ package k8s
 import (
 	"context"
 	"errors"
+	"fmt"
 	"time"
 
 	"github.com/stretchr/testify/require"
@@ -13,6 +14,46 @@ import (
 	"github.com/gruntwork-io/terratest/modules/retry"
 	"github.com/gruntwork-io/terratest/modules/testing"
 )
+
+// AssertAnyNodeHasLabelE will return an error if no nodes are found with the given label.
+func AssertAnyNodeHasLabelE(t testing.TestingT, options *KubectlOptions, label string) error {
+	nodes, err := GetNodesByFilterE(t, options, metav1.ListOptions{
+		LabelSelector: label,
+	})
+
+	require.NoError(t, err)
+	if len(nodes) == 0 {
+		return fmt.Errorf("No nodes found with label %s", label)
+	}
+	return nil
+}
+
+// AssertAnyNodeHasLabel will fail the test if no nodes are found with the given label.
+func AssertAnyNodeHasLabel(t testing.TestingT, options *KubectlOptions, label string) {
+	err := AssertAnyNodeHasLabelE(t, options, label)
+	require.NoError(t, err)
+}
+
+func AssertAnyNodeHasTaintE(t testing.TestingT, options *KubectlOptions, taintKey string, taintValue string, taintEffect corev1.TaintEffect) error {
+	nodes, err := GetNodesE(t, options)
+	require.NoError(t, err)
+
+	for _, node := range nodes {
+		for _, taint := range node.Spec.Taints {
+			if taint.Key == taintKey && taint.Value == taintValue && taint.Effect == taintEffect {
+				return nil
+			}
+		}
+	}
+
+	return fmt.Errorf("No nodes found with taint %s/%s/%s", taintKey, taintValue, taintEffect)
+}
+
+// AssertAnyNodeHasTaint will fail the test if no nodes are found with the given taint.
+func AssertAnyNodeHasTaint(t testing.TestingT, options *KubectlOptions, taintKey string, taintValue string, taintEffect corev1.TaintEffect) {
+	err := AssertAnyNodeHasTaintE(t, options, taintKey, taintValue, taintEffect)
+	require.NoError(t, err)
+}
 
 // GetNodes queries Kubernetes for information about the worker nodes registered to the cluster. If anything goes wrong,
 // the function will automatically fail the test.
